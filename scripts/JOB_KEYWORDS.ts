b6c5afda40da2421aa28cbb9d1a8b790663862b1
alfa546 <noumansajid623@gmail.com>
2026-01-19 22:39:29 +0500

    Finalizing JOB SEARCHER FILES

--- scripts/JOB_KEYWORDS.ts
+++ scripts/JOB_KEYWORDS.ts
@@ -1,8 +1,9 @@
-// Job keywords to search for in Pakistan
+// Jobs keywords to search for in Pakistan
 export const JOB_KEYWORDS = [
   'Teaching Jobs in Pakistan',
   'IT Jobs in Pakistan',
   'Doctor Jobs Lahore',
   'Scholarship Jobs in Pakistan',
   'Engineering Jobs in Pakistan',
+  'Veterinary Jobs in Pakistan',
 ];
